4d173dc362bbe0d2934fab5607e2a35a4a446a80
fmedico <fmedico@car-tor-000100.carver-as.com>
2022-03-17 12:49:02 +0100

    Added CRUD to backend

--- src/main/java/com/demothefirstspring/the/first/models/services/ClienteServiceImpl.java
+++ src/main/java/com/demothefirstspring/the/first/models/services/ClienteServiceImpl.java
@@ -20,5 +20,24 @@ public class ClienteServiceImpl implements IClienteService{
 		// TODO Auto-generated method stub
 		return (List<Cliente>) clienteDao.findAll();
 	}
+	@Override
+	@Transactional(readOnly=true)
+	public Cliente findById(Long id) {
+		// TODO Auto-generated method stub
+		return clienteDao.findById(id).orElse(null);
+	}
+	@Override
+	@Transactional
+	public Cliente save(Cliente cliente) {
+		// TODO Auto-generated method stub
+		return clienteDao.save(cliente);
+	}
+	@Override
+	@Transactional
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		clienteDao.deleteById(id);
+		
+	}
 
 }
